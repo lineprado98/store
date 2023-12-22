a525a8f15c713a6be5a14155db60f1257de74c67
Aline Prado Barbosa <aline_100lika@hotmail.com>
2023-12-22 10:13:15 -0300

    feat:Dependency packages

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage();
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
